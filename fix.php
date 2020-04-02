ce5fa8eaa33e07b18a917998a43077927ce5234a
kattyku02 <63057875+kattyku02@users.noreply.github.com>
2020-04-02 23:40:02 +0700

    Update fix.php

--- fix.php
+++ fix.php
@@ -70,7 +70,7 @@ function change(){
         sleep(1);
         }
         sleep(3);
-        $boba10 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"GOFOOD022620A"}');
+        $boba10 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD010420B"}');
         $messageboba10 = fetch_value($boba10,'"message":"','"');
         if(strpos($boba10, 'Promo kamu sudah bisa dipakai.')){
         echo "\n".color("green","+] Message: ".$messageboba10);
